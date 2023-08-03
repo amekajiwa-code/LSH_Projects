ef6fed8fd50236b85df5ef8fdbfa0bd61547d357
songha <paperstory10@naver.com>
2023-08-03 10:12:11 +0900

    asd

--- MyOctTree/main.cpp
+++ MyOctTree/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main() {
 	Vector3 frontTopLeft(0.0f, 0.0f, 0.0f);
-	Vector3 backBottomRight(800.0f, 600.0f, 1.0f);
-	double width = 800, height = 600, depth = 2;
+	Vector3 backBottomRight(800.0f, 600.0f, 100.0f);
+	double width = 800, height = 600, depth = 100;
 	MyQuadrant* myTree = new MyQuadrant(frontTopLeft, backBottomRight, width, height, depth);
 
 	myTree->BuildTree();
