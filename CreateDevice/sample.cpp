a6d928cda2bd8bd9613910e9dd8171dda2a4ef85
songha <paperstory10@naver.com>
2023-08-10 19:00:53 +0900

    스왑체인 디바이스 클래스 분리

--- CreateDevice/sample.cpp
+++ CreateDevice/sample.cpp
@@ -1,40 +1,57 @@
 #include "sample.h"
 
-bool sample::Init() {
-    DXGI_SWAP_CHAIN_DESC SwapChainDesc; // 스왑체인 속성값 구조체: 더블버퍼링을 구현하는데 사용됨
+HRESULT MyDevice::CreateDevice() {
+    HRESULT hResult;
+
+    UINT Flags = 0;
+    D3D_DRIVER_TYPE DriverType = D3D_DRIVER_TYPE_HARDWARE;
+    D3D_FEATURE_LEVEL FeatureLevels = D3D_FEATURE_LEVEL_11_0;
+
+    if (FAILED(hResult)) {
+        return false;
+    }
+
+    return S_OK;
+}
+
+HRESULT MyDevice::CreateFactory() {
+    HRESULT hResult;
+    if (FAILED(hResult = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)(&mFactory)))) {
+        return false;
+    }
+    return hResult;
+}
+
+HRESULT MyDevice::CreateSwapChain() {
+    HRESULT hResult = S_OK;
+    if (mFactory == NULL) return S_FALSE;
+
+    DXGI_SWAP_CHAIN_DESC SwapChainDesc;
     ZeroMemory(&SwapChainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
-    SwapChainDesc.BufferDesc.Width = 800; // 버퍼 크기
-    SwapChainDesc.BufferDesc.Height = 600;
-    SwapChainDesc.BufferDesc.RefreshRate.Numerator = 60; // 버퍼 주사율 분자
-    SwapChainDesc.BufferDesc.RefreshRate.Denominator = 1; // 버퍼 주사율 분모
-    SwapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // 색 포멧
-    SwapChainDesc.SampleDesc.Count = 1; // 백 버퍼 멀티샘플링 count
-    SwapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT; // 렌더 타겟
+    SwapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
     SwapChainDesc.BufferCount = 1;
     SwapChainDesc.OutputWindow = mHWnd;
     SwapChainDesc.Windowed = true;
+    SwapChainDesc.BufferDesc.Width = 800;
+    SwapChainDesc.BufferDesc.Height = 600;
+    SwapChainDesc.BufferDesc.RefreshRate.Numerator = 60;
+    SwapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+    SwapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    SwapChainDesc.SampleDesc.Count = 1;
 
-    D3D_DRIVER_TYPE DriverType = D3D_DRIVER_TYPE_HARDWARE;
-    UINT Flags = 0;
-    D3D_FEATURE_LEVEL FeatureLevels = D3D_FEATURE_LEVEL_11_0; // 최소 버전 DX11
+    if (FAILED(hResult = mFactory->CreateSwapChain(mDevice, &SwapChainDesc, &mSwapChain))); {
+        return hResult;
+    }
 
-    //1) 디바이스 설정
-    HRESULT hResult = D3D11CreateDeviceAndSwapChain(NULL, DriverType, NULL, Flags, &FeatureLevels, 1, D3D11_SDK_VERSION,
-        &SwapChainDesc,
-        &mSwapChain, // 백버퍼 인터페이스
-        &mDevice, // dx 인터페이스 (생성)
-        NULL,
-        &mImmediateContext // dx 인터페이스 (관리)
-    );
+    return hResult;
+}
 
-    if (FAILED(hResult)) return false;
-
-    //2) 백버퍼 가져옴
-    ID3D11Texture2D* backBuffer; // 백버퍼 포인터
-    hResult = mSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D)/*인터페이스 유형*/ , (LPVOID*)&backBuffer);//스왑체인의 백버퍼 가져옴
+HRESULT MyDevice::SetRenderTargetView() {
+    HRESULT hResult;
+    ID3D11Texture2D* backBuffer;
+    hResult = mSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&backBuffer);
 
     if (SUCCEEDED(hResult)) {
-        //3) 렌더타겟 지정
         hResult = mDevice->CreateRenderTargetView(backBuffer, NULL, &mRenderTargetView);
 
         if (FAILED(hResult)) {
@@ -42,9 +59,26 @@ bool sample::Init() {
             return false;
         }
     }
-    
-    //4) 시연
-    backBuffer->Release();
+}
+
+HRESULT MyDevice::SetViewPort() {
+    HRESULT hResult = S_OK;
+    mViewPort.Width = static_cast<float>(mDwWindowWidth);
+    mViewPort.Height = static_cast<float>(mDwWindowHeight);
+    mViewPort.MinDepth = 0.0f;
+    mViewPort.MaxDepth = 1.0f;
+    mViewPort.TopLeftX = 0;
+    mViewPort.TopLeftY = 0;
+    mImmediateContext->RSSetViewports(1, &mViewPort); // 레스터라이저
+    return S_OK;
+}
+
+bool sample::Init() {
+    CreateDevice();
+    CreateFactory();
+    CreateSwapChain();
+    SetRenderTargetView();
+    SetViewPort();
     return true;
 };
 
@@ -53,7 +87,7 @@ bool sample::Frame() {
 };
 
 bool sample::Render() {
-    float color[4] = { 1.0f , 0, 0, 1.0f };
+    float color[4] = { 1.0f , 0.5f, 0.5f, 1.0f };
     mImmediateContext->ClearRenderTargetView(mRenderTargetView, color);
     // 지형, 캐릭터, 오브젝트, 이펙트, 인터페이스
     HRESULT hResult = mSwapChain->Present(0, 0);
@@ -66,8 +100,21 @@ bool sample::Render() {
 bool sample::Release() {
     if (mSwapChain) mSwapChain->Release();
     if (mDevice) mDevice->Release();
-    if (mImmediateContext) mImmediateContext->Release();
+
+    if (mImmediateContext) {
+        mImmediateContext->ClearState();
+        mImmediateContext->Release();
+    }
+
     if (mRenderTargetView) mRenderTargetView->Release();
+    if (mFactory) mFactory->Release();
+
+    mFactory = nullptr;
+    mDevice = nullptr;
+    mImmediateContext = nullptr;
+    mSwapChain = nullptr;
+    mRenderTargetView = nullptr;
+
     return true; 
 };
 
--- CreateDevice2/sample.h
+++ CreateDevice2/sample.h
@@ -2,32 +2,25 @@
 #include "Core.h"
 #include <DXGI.h>
 
-class MyDevice : public Core {
-public:
+class MyDevice {
+	
+};
+
+class MySwapChain {
+	
+};
+
+class sample : public Core {
+private:
 	IDXGIFactory* mFactory = nullptr;
 	ID3D11Device* mDevice = nullptr;
 	ID3D11DeviceContext* mImmediateContext = nullptr;
 	IDXGISwapChain* mSwapChain = nullptr;
 	ID3D11RenderTargetView* mRenderTargetView = nullptr;
 	D3D11_VIEWPORT mViewPort;
-
-	MyDevice() {};
-	~MyDevice() {};
-
-	HRESULT CreateDevice();
-	HRESULT CreateFactory();
-	HRESULT CreateSwapChain();
-	HRESULT SetRenderTargetView();
-	HRESULT SetViewPort();
-};
-
-class sample : public MyDevice {
 public:
 	bool  Init();
 	bool  Frame();
 	bool  Render();
 	bool  Release();
-
-	sample() {}
-	~sample() {}
 };
